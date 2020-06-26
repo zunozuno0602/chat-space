a7a04274912e9f50197a271bba975c3485d0f71d
zunozuno0602 <greeeen-disneylove-yuu@ezweb.ne.jp>
2020-06-26 21:33:00 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'messages/index'
+
+  root "messages#index"
+  
+  get 'shared/side_bar'
+
+  root 'shared#side_bar'
 end
