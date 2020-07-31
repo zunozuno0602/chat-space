4ae52724e1c15025f79ec447d7d8afab5ce18893
zunozuno0602 <greeeen-disneylove-yuu@ezweb.ne.jp>
2020-07-31 17:45:28 +0900

    変更後２

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -56,11 +56,12 @@ $(function(){
       $('.messages').append(html);
       $('form')[0].reset();
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      $('.form__submit').prop('disabled', false);
     })
     .fail(function(){
-         alert("メッセージ送信に失敗しました");
-         $('.form__submit').prop('disabled', false);
+      alert("メッセージ送信に失敗しました");
+    })
+    .always(function(){
+      $('.form__submit').prop('disabled', false);
     });
   })
 });
