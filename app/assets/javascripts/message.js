3a2b5037cf1220dfd08d479ec49e74f053af3b78
zunozuno0602 <greeeen-disneylove-yuu@ezweb.ne.jp>
2020-08-27 21:25:21 +0900

    挙動確認３

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,68 @@
 $(function(){ 
+  
   function buildHTML(message){
-   if ( message.image ) {
-     var html =
+    if ( message.image ) {
+      var html =
       `<div class="message">
+      <div class="upper-message">
+      <div class="upper-message__user-name">
+             ${message.user_name}
+             </div>
+           <div class="upper-message__date">
+           ${message.created_at}
+           </div>
+           </div>
+           <div class="lower-message">
+           <p class="lower-message__content">
+           ${message.content}
+           </p>
+           </div>
+           <img src=${message.image} >
+           </div>`
+           return html;
+          } else {
+     var html =
+     `<div class="message">
          <div class="upper-message">
            <div class="upper-message__user-name">
              ${message.user_name}
            </div>
            <div class="upper-message__date">
              ${message.created_at}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
+             </div>
+             </div>
+             <div class="lower-message">
+             <p class="lower-message__content">
              ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
+             </p>
+             </div>
+             </div>`
      return html;
-   } else {
-     var html =
-      `<div class="message">
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
+    };
  }
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -64,4 +88,7 @@ $(function(){
       $('.form__submit').prop('disabled', false);
     });
   })
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
