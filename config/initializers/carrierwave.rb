4b77cd9d64865a935d6a316fa97f3f2ac2695136
zunozuno0602 <greeeen-disneylove-yuu@ezweb.ne.jp>
2020-08-27 20:10:30 +0900

    挙動確認２

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'yuu-chat-space'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/yuu-chat-space'
 end
